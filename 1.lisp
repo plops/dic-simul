6b445dfdabc027934aeb16be058e11f882bdb23f
mr chickenfarmer <martin@torben.(none)>
2009-12-10 14:42:27 +0000

    added simulation of how the dic image would actually look like

--- 1.lisp
+++ 1.lisp
@@ -91,42 +91,80 @@ chosen."
       (write-sequence buf s)))
   (values maxi mini))
 
-
 (defparameter lut
   (make-array '(256 256) :element-type 'double-float)
   "Storage for the lookup table of eta values.")
 
+(let ((max-voltage 30)
+      (image-factor .75)
+      (max-value 255)
+      (a 0.0113608) ; nm/V^3
+      (b 4.31718))  ; nm
+
+ (defun gray-value->d (g)
+   "convert gray value g into a voltage v and then into mirror deflection d/um" 
+   (let* ((v (* max-voltage image-factor (/ g max-value)))
+	  (v3 (* v v v)))
+     (* 1/1000 (+ (* a v3) b))))
+
+ (defun d->gray-value (d)
+   "Convert deflection d/um into a gray value g/ADU."
+   (let* ((arg (* (/ (- (* d 1000) b) a)))
+	  (arg^1/3 (expt arg 1/3)))
+     (* arg^1/3 max-value (/ (* max-voltage image-factor))))))
+
+(defun sinc (x)
+  "Sinc function sin(x)/x."
+  (if (= x 0d0)
+      1d0
+      (/ (sin x) x)))
+
+(gray-value->d 255) ; should be .133
+(d->gray-value .133) ; 254.5
+
+(with-open-file (sm "/dev/shm/gv-d.dat"
+		    :direction :output
+		    :if-does-not-exist :create
+		    :if-exists :overwrite)
+  (dotimes (i 256)
+    (format sm "~d ~a~%" i (gray-value->d i))))
+
+(with-open-file (sm "/dev/shm/d-gv.dat"
+		    :direction :output
+		    :if-does-not-exist :create
+		    :if-exists :overwrite)
+  (loop for i from 5 upto 133 do
+    (format sm "~d ~a~%" i (d->gray-value (/ i 1000)))))
+
+
+
+(defun torsion-mirror-intensity (delta-d l)
+  "Given a difference of deflections of neighbouring mirrors delta-d
+and wavelength l determine the intensity after DIC method."
+  (let* ((k (* 2 pi (/ l))))
+    (- 1 (sinc (* k delta-d)))))
+
+(torsion-mirror-intensity .133 .480) ; .4339
+
 (defun calc-lut ()
   (let* ((ll 16d0)
 	 (l .480))
-    (labels ((uce->d (x) ; convert voltage U_CE=x V into mirror
-			 ; deflection d/um
-	       (let* ((x3 (* x x x))
-		      (a 0.0113608)
-		      (d 4.31718))
-		 (* 1/1000 (+ (* a x3) d)))))
-      (let* ((imagefactor .75)
-	     (maxvoltage 30)
-	     (max-deflection (uce->d (* imagefactor maxvoltage)))
-	     (max-piston-intensity (- 1 (cos (* 2 2 pi (/ l)
-						max-deflection)))))
-	;; max-deflection must be 133 nm
-	;; (format t "~s~%" (list "max-defl" max-deflection))  
-	(img-do (lut)
-		(let* ((d1 (uce->d (* x imagefactor maxvoltage)))
-		       (d2 (uce->d (* y imagefactor maxvoltage)))
-		       (delta-d (- d1 d2))
-		       (arg (* 2 pi delta-d (/ l)))
-		       (torsion-mirror-intensity (- 1 (if (= arg 0d0)
-							  1d0
-							  (/ (sin arg) arg)))))
-		  (setf (aref lut i j) (/ torsion-mirror-intensity
-					  max-piston-intensity))))))))
+    (let* ((max-deflection (gray-value->d 255))
+	   (k (* 2 pi (/ l)))
+	   (max-piston-intensity (- 1 (cos (* k 2 max-deflection)))))
+      ;; max-deflection must be 133 nm
+      (format t "~s~%" (list "max-defl in um" max-deflection
+			     "max-piston-intensity" max-piston-intensity))  
+      (img-do (lut)
+	(let* ((d1 (gray-value->d i)) ; i goes from 0 to 255
+	       (d2 (gray-value->d j))
+	       (delta-d (- d1 d2)))
+	  (setf (aref lut i j) (/ (torsion-mirror-intensity delta-d l)
+				  max-piston-intensity)))))))
   
 
 (calc-lut)
 (write-pgm "/dev/shm/lut.pgm" lut) ;; output lut for reference
-
 
 (defun read-without-comment (s)
   "Read the next data from stream S, skipping any comments."
@@ -159,12 +197,13 @@ chosen."
 	      (coerce (aref data (+ i (* w j))) 'double-float)))
       bild)))
 
-
 (defparameter erika (normalize (read-pgm "erika.pgm"))
-  "Image that we want to generate on the camera")
+  "Read input image.")
 
 ; I need some black region for focussing
+; and also i want to scale the image values
 (img-do (erika)
+  (setf (aref erika i j) (* .2 (aref erika i j)))
   (when (< (let ((x1 (- x .57))
 		 (y1 (- y .35)))
 	     (+ (* x1 x1) (* y1 y1))) .001)
@@ -205,6 +244,28 @@ together with its predecessor in x direction."
 
 (write-pgm "/dev/shm/erika.pgm" erika)   ;; image that should be on the camera
 (write-pgm "/dev/shm/erika2.pgm" erika2) ;; image that is displayed on the MMA
+
+;; test this image by doing the inverse transform:
+(defparameter erika2a (make-array '(256 256) :element-type 'double-float)
+  "Displayed deflections on the MMA.")
+(img-do (erika2a)
+  (setf (aref erika2a i j)
+	(gray-value->d (aref erika2 i j))))
+
+(write-pgm "/dev/shm/erika2a-deflections.pgm" erika2a)
+
+(defparameter erika2b (make-array '(256 256) :element-type 'double-float)
+  "Intensity of the DIC image.")
+
+(img-do (erika2a)
+  (setf (aref erika2b i j)
+	(if (eq i 255)
+	    0d0
+	    (torsion-mirror-intensity (- (aref erika2a i j)
+					 (aref erika2a (1+ i) j))
+				      .480))))
+
+(write-pgm "/dev/shm/erika2b-dic.pgm" erika2b)
 
 ;; Now only some boring conversion routines. The MMA software expects
 ;; 24-bit color BGR BMP and reads the blue data.
